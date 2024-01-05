f49dd0221b0c69064282f94ac1de504e90cc46eb
nija10 <nijakharangar@gmail.com>
2024-01-05 15:49:32 +0530

    add basic search engine (no API)

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSearchSubmit);
