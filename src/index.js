a00d20588c7568fca0e00dc2ddfd2ff4f87c0a2d
nija10 <nijakharangar@gmail.com>
2024-01-09 23:13:09 +0530

    Injecting icon element

--- src/index.js
+++ src/index.js
@@ -7,6 +7,13 @@ function refreshWeather(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
+
+  iconElement.innerHTML = `<img
+      src=
+      "${response.data.condition.icon_url}"
+      class="weather-app-icon"
+    />`;
 
   temperatureElement.innerHTML = Math.round(temperature);
   cityElement.innerHTML = response.data.city;
